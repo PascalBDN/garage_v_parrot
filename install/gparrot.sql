21f64607e377d49057b2a4c3a6833882637cb59a
PascalBDN <patrick.beaudemoulin@protonmail.com>
2023-07-02 10:42:19 +0200

    update sql

--- install/gparrot.sql
+++ install/gparrot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : jeu. 29 juin 2023 à 09:30
+-- Généré le : dim. 02 juil. 2023 à 08:39
 -- Version du serveur : 5.7.39
 -- Version de PHP : 8.2.0
 
@@ -91,7 +91,6 @@ CREATE TABLE `cars` (
 --
 
 INSERT INTO `cars` (`id`, `img`, `modele`, `prix`, `annee`, `energie`, `kilometrage`, `description`, `securite`, `places`, `options_list`, `create_at`) VALUES
-(17, 'images/ambreetmoi_64954a352c2913.10296835.jpeg', 'citronopipo', '250.00', 1987, 'Essence', 123, 'SUPERBE', NULL, NULL, NULL, NULL),
 (22, 'images/mercedes-1327610_640_64992f8a74f0a9.37070857.jpg', 'Modèle 1', '15000.00', 2022, 'Essence', 10000, 'Description du modèle 1', NULL, NULL, NULL, NULL),
 (23, 'images/car-4445171_640_64992f3ebca350.56695919.jpg', 'Modèle 2', '20000.00', 2021, 'Diesel', 5000, 'Description du modèle 2', NULL, NULL, NULL, NULL),
 (24, 'images/piaggio-ape-7254648_640_64992f205619d2.67122535.jpg', 'Modèle 3', '18000.00', 2020, 'Essence', 8000, 'Description du modèle 3', NULL, NULL, NULL, NULL),
@@ -107,11 +106,12 @@ INSERT INTO `cars` (`id`, `img`, `modele`, `prix`, `annee`, `energie`, `kilometr
 (34, 'images/buick-1400243_640_64992d791634c8.57486595.jpg', 'Modèle 13', '22000.00', 2018, 'Essence', 9000, 'Description du modèle 13', NULL, NULL, NULL, NULL),
 (35, 'images/car-1880381_640_64992d596c3543.00257668.jpg', 'Modèle 14', '25000.00', 2021, 'Diesel', 11000, 'Description du modèle 14', NULL, NULL, NULL, NULL),
 (36, 'images/car-604019_640_64992d10486e67.87438648.jpg', 'Modèle 15', '18000.00', 2019, 'Essence', 7000, 'Description du modèle 15', NULL, NULL, NULL, NULL),
-(37, 'images/renault-juvaquatre-1661009_640_64992cd3aa6d01.72340400.jpg', 'Modèle 16', '24000.00', 2022, 'Hybride', 10000, 'Description du modèle 16', NULL, NULL, NULL, NULL),
+(37, 'images/renault-juvaquatre-1661009_640_64992cd3aa6d01.72340400.jpg', 'Modèle 16', '24000.00', 2022, 'essence', 10000, 'Description du modèle 16', 'Antidémarrage', '2', NULL, NULL),
 (38, 'images/auto-2179220_640_64992caccc7df2.92134810.jpg', 'Modèle 17', '20000.00', 2020, 'Essence', 8000, 'Description du modèle 17', NULL, NULL, NULL, NULL),
 (39, 'images/car-1300629_640_64992c87286bc2.70929675.png', 'Modèle 18', '23000.00', 2021, 'Diesel', 6000, 'Description du modèle 18', NULL, NULL, NULL, NULL),
 (40, 'images/auto-788747_640_64992c5a2c9964.15517462.jpg', 'Modèle 19', '21000.00', 2018, 'Essence', 12000, 'Description du modèle 19', NULL, NULL, NULL, NULL),
-(41, 'images/oldtimer-1197800_640_64992bf9b8b646.31898218.jpg', 'auto pipo', '19000.00', 2019, 'electrique', 9000, 'Description du modèle 20', 'Airbags', '4', NULL, NULL);
+(41, 'images/oldtimer-1197800_640_64992bf9b8b646.31898218.jpg', 'auto pipo', '19000.00', 2019, 'electrique', 9000, 'Description du modèle 20', 'Airbags', '4', NULL, NULL),
+(42, 'images/Image 12-04-2023 à 13.12 2_649fcd90a1a1b7.97015340.jpg', 'sqdfqs', '11.00', 11, 'essence', 11, '134', 'Airbags', '2', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -133,7 +133,7 @@ CREATE TABLE `Horaires` (
 --
 
 INSERT INTO `Horaires` (`id`, `jour`, `ouverture_matin`, `fermeture_matin`, `ouverture_apresmidi`, `fermeture_apresmidi`) VALUES
-(1, 'Lundi', '00:00:00', '00:00:00', '16:00:00', '19:00:00'),
+(1, 'Lundi', '12:00:00', '13:00:00', '16:00:00', '19:00:00'),
 (2, 'Mardi', '08:00:00', '12:00:00', '14:00:00', '18:00:00'),
 (3, 'Mercredi', '08:00:00', '12:00:00', '14:00:00', '18:00:00'),
 (4, 'Jeudi', '08:00:00', '12:00:00', '14:00:00', '18:00:00'),
@@ -171,6 +171,7 @@ INSERT INTO `services` (`id`, `nom`, `description`) VALUES
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('admin','staff') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -179,10 +180,11 @@ CREATE TABLE `users` (
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
-(26, 'adminUser', '$2y$10$5NziUWcWeyo2fQdZAmN0quG2.ZVsXqmUj7h4B2PCik0jOxf.i5zUG', 'admin'),
-(27, 'staff1', '$2y$10$JMhKm9nFWrE9zqQBxmUb8OmIy3C71Wa3Ild53hw2qnL6N6ankGkN6', 'staff'),
-(28, 'pascal', '$2y$10$W4mCmYHO0x180z.L5sDx1.lckqp2jQiAFn7grgUhfIzgVxWXZJ54i', 'staff');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`) VALUES
+(26, 'adminUser', NULL, '$2y$10$5NziUWcWeyo2fQdZAmN0quG2.ZVsXqmUj7h4B2PCik0jOxf.i5zUG', 'admin'),
+(31, 'piboul', NULL, '$2y$10$34.pSFnmsfK.Wx01lAFtte3nwweE5R.8D2yNk0AirL8AKYNHESweS', 'admin'),
+(33, 'yop', 'yop@yop.fr', '$2y$10$Oqp.IICiXKDZW3a/WnucIO4IrgawvQhnecyGvdqphgXMbKeZWHSdG', 'staff'),
+(34, 'admin', 'admin@admin.fr', '$2y$10$jP.J28NNmbavTVU9PJss1OrCHbRcpmyESlHVFiviAYRHa0BLol7Ve', 'admin');
 
 --
 -- Index pour les tables déchargées
@@ -233,7 +235,7 @@ ALTER TABLE `avis_clients`
 -- AUTO_INCREMENT pour la table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT pour la table `Horaires`
@@ -245,13 +247,13 @@ ALTER TABLE `Horaires`
 -- AUTO_INCREMENT pour la table `services`
 --
 ALTER TABLE `services`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
